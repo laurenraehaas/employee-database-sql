f09d9a6a4947e2d3c7228ebcc32d342dd7296afc
lauren haas <laurenraehaas@gmail.com>
2023-07-20 13:18:44 -0500

    adding to seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (name)
+VALUES ('Engineering') ('Operations')
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Software Engineer', 80000, 101) ('Data Analyst', 65000, 102) ('Product Manager', 95000, 103)
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Smith', 01, NULL) ('Jane', 'Doe', 02, 2) ('Michael', 'Johnson', 03, 3)
